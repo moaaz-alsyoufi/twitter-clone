f23d5621330ed0a13e56064121669184d3e5d976
moaaz_alsyoufi <moaaz.syf@gmail.com>
2022-09-01 11:01:41 +0300

    Update: main header completed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const leftSidebar = document.querySelector(".tc-left-sidebar")
 const rightSidebar = document.querySelector(".tc-right-sidebar")
 const header = document.querySelector(".tc-content__main-header__main")
+const content = document.querySelector(".tc-content__inner")
 let leftSidebarX = leftSidebar.offsetLeft
 let rightSidebarLeft = rightSidebar.offsetLeft
 let leftSidebarWidth = leftSidebar.clientWidth
 let rightSidebarWidth = rightSidebar.clientWidth
+
+content.addEventListener('wheel', (e) => {
+  console.log(content.clientTop);
+})
 
 
 header.style.left = leftSidebarX + leftSidebarWidth
